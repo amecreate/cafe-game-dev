784619041e4467aec2eedfcbdfc1c2113ede303a
amecreate <amelieasmile@gmail.com>
2021-07-07 12:58:15 -0400

    add return button v1

--- js/SceneDrink.js
+++ js/SceneDrink.js
@@ -9,6 +9,7 @@ export default class SceneDrink extends Phaser.Scene {
         this.load.image('drink2', 'assets/Drink2.png');
         this.load.image('drink3', 'assets/Drink3.png');
         this.load.image('drink4', 'assets/Drink4.png');
+        this.load.image('cake1', 'assets/Cake1.png');
     }
 
     create() {
@@ -18,36 +19,67 @@ export default class SceneDrink extends Phaser.Scene {
         // randomly select a drink from drinkList
         this.drink = this.physics.add.image(this.cameras.main.centerX, this.cameras.main.centerY, Phaser.Math.RND.pick(drinkList));
 
-        const cakeButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-200, 'Cheese Cake Please!', { 
+        this.cakeButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-210, 'Cheese Cake Please!', { 
             fontSize: '20px', 
             fontFamily: 'Helvetica', 
             color: '#000000', 
             backgroundColor: '#FEA99F', 
             padding: {left:3,right:3,top:3,bottom:3}})
             .setOrigin(0.5).setInteractive();
+
         
         // toggle text transparency
-        cakeButton.on('pointerover', pointer => {
-            cakeButton.alpha = 0.5
-        });
-
-        cakeButton.on('pointerout', pointer => {
-            cakeButton.alpha = 1
-        });
+        this.cakeButton.on('pointerover', pointer => {
+            this.cakeButton.alpha = 0.5});
+            
+        this.cakeButton.on('pointerout', pointer => {
+            this.cakeButton.alpha = 1});
         
-        cakeButton.once('pointerup', function (pointer) {
-            cakeButton.destroy();
+        // cake button click event
+        this.cakeButton.once('pointerup', function (pointer) {
+            this.cakeButton.destroy();
 
+            // move existing sprite
             this.tweens.add({
                 targets: this.drink,
-                x: this.cameras.main.centerX - 300,
+                x: this.cameras.main.centerX - 200,
                 duration: 2000,
                 ease: 'Power2',
                 yoyo: false
             });
 
+            // delay add new sprite
+            this.time.delayedCall(2000, () => {
+                this.add.sprite(this.cameras.main.centerX+200, this.cameras.main.centerY, "cake1");
+
+                // delay add return button
+                this.time.delayedCall(500, () => {
+                    this.returnButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-210, 'Return', { 
+                        fontSize: '20px', 
+                        fontFamily: 'Helvetica', 
+                        color: '#000000', 
+                        backgroundColor: '#FEA99F', 
+                        padding: {left:3,right:3,top:3,bottom:3}})
+                        .setOrigin(0.5).setInteractive();
+                    
+                    // return button toggle
+                    this.returnButton.on('pointerover', pointer => {
+                        this.returnButton.alpha = 0.5});
+                        
+                    this.returnButton.on('pointerout', pointer => {
+                        this.returnButton.alpha = 1});
+
+                    // return button click event
+                    this.returnButton.once('pointerup', pointer => {
+                        this.scene.start('SceneWelcome')
+                    })
+
+                    });
+            });
+
 /*             this.moveDrink = this.physics.moveTo(this.drink, this.cameras.main.centerX-100, this.cameras.main.centerY, 200); */
         },this);
+
     }
 
     update () {
