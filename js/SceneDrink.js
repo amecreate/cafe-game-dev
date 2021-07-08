bc078b02bb2b58cd51828dcb12fa761ce69fbfa2
amecreate <amelieasmile@gmail.com>
2021-07-08 09:05:45 -0400

    added return button

--- js/SceneDrink.js
+++ js/SceneDrink.js
@@ -19,13 +19,15 @@ export default class SceneDrink extends Phaser.Scene {
         // randomly select a drink from drinkList
         this.drink = this.physics.add.image(this.cameras.main.centerX, this.cameras.main.centerY, Phaser.Math.RND.pick(drinkList));
 
-        this.cakeButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-210, 'Cheese Cake Please!', { 
+        // add buttons
+        const textConfig = { 
             fontSize: '20px', 
             fontFamily: 'Helvetica', 
             color: '#000000', 
             backgroundColor: '#FEA99F', 
-            padding: {left:3,right:3,top:3,bottom:3}})
-            .setOrigin(0.5).setInteractive();
+            padding: {left:8,right:8,top:3,bottom:3}};
+        this.cakeButton = this.add.text(this.cameras.main.centerX-43, this.cameras.main.centerY-210, 'Cheese Cake Please!', textConfig).setOrigin(0.5).setInteractive();
+        this.returnButton = this.add.text(this.cameras.main.centerX+110, this.cameras.main.centerY-210, 'Return', textConfig).setOrigin(0.5).setInteractive();
 
         
         // toggle text transparency
@@ -35,9 +37,20 @@ export default class SceneDrink extends Phaser.Scene {
         this.cakeButton.on('pointerout', pointer => {
             this.cakeButton.alpha = 1});
         
+        this.returnButton.on('pointerover', pointer => {
+            this.returnButton.alpha = 0.5});
+            
+        this.returnButton.on('pointerout', pointer => {
+            this.returnButton.alpha = 1});
+        
+        // return button click event
+        this.returnButton.once('pointerup', pointer => {
+            this.scene.start('SceneWelcome')});
+
         // cake button click event
         this.cakeButton.once('pointerup', function (pointer) {
-            this.cakeButton.destroy();
+            // change text
+            this.cakeButton.setText('Enjoy!');
 
             // move existing sprite
             this.tweens.add({
@@ -51,30 +64,6 @@ export default class SceneDrink extends Phaser.Scene {
             // delay add new sprite
             this.time.delayedCall(2000, () => {
                 this.add.sprite(this.cameras.main.centerX+200, this.cameras.main.centerY, "cake1");
-
-                // delay add return button
-                this.time.delayedCall(500, () => {
-                    this.returnButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-210, 'Return', { 
-                        fontSize: '20px', 
-                        fontFamily: 'Helvetica', 
-                        color: '#000000', 
-                        backgroundColor: '#FEA99F', 
-                        padding: {left:3,right:3,top:3,bottom:3}})
-                        .setOrigin(0.5).setInteractive();
-                    
-                    // return button toggle
-                    this.returnButton.on('pointerover', pointer => {
-                        this.returnButton.alpha = 0.5});
-                        
-                    this.returnButton.on('pointerout', pointer => {
-                        this.returnButton.alpha = 1});
-
-                    // return button click event
-                    this.returnButton.once('pointerup', pointer => {
-                        this.scene.start('SceneWelcome')
-                    })
-
-                    });
             });
 
 /*             this.moveDrink = this.physics.moveTo(this.drink, this.cameras.main.centerX-100, this.cameras.main.centerY, 200); */
